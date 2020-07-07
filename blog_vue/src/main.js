c7c90a718e829e4e38f83c8f3b6561d7a8f931e5
urchin <648453059@qq.com>
2020-07-07 20:24:52 +0800

    修改控件

--- blog_vue/src/main.js
+++ blog_vue/src/main.js
@@ -2,9 +2,10 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-import vuetify from "./plugins/vuetify";
 import "roboto-fontface/css/roboto/roboto-fontface.css";
 import "@fortawesome/fontawesome-free/css/all.css";
+import "vuetify/dist/vuetify.min.css";
+import vuetify from "./plugins/vuetify";
 
 Vue.config.productionTip = false;
 
